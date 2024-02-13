a726542f9509035c329f13e91480329b179564e1
Jake <jakeogrady15@gmail.com>
2024-02-13 18:34:43 +0000

    Added loop

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ soup = BeautifulSoup(r.content, 'lxml')
 headings = soup.find_all("div",class_="film")
 for h in headings:
     title = h.find("h3")
-    print(title.text)
+    time = h.find("div",class_="times")
+    times = time.find("a")
+    if times:
+         print(f'{title.text}: {times.text}')
+    
+   
