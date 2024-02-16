1753b75dbf0e5b32eb3e72c38a08b55df380f804
Jake <jakeogrady15@gmail.com>
2024-02-16 19:33:49 +0000

    Reinstated If-Statement

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ url = "https://www.palas.ie/"
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'lxml')
 
-
-list = soup.find_all("div", class_="film")
-
-for film in list:
-    str = ""
-    title = film.find("h3").find("a")
-    str += title.text+":"
-    timeSlots = film.find("div",class_="times")
-    times = timeSlots.find_all("a")
-    for time in times:
-        str+= " " + time.text
-    print(str)
+def displayFilms():
+    list = soup.find_all("div", class_="film")
+    for film in list:
+        str = ""
+        title = film.find("h3").find("a")
+        
+        timeSlots = film.find("div",class_="times")
+        times = timeSlots.find_all("a")
+        if times:
+            str += title.text+":"
+            for time in times:
+                str+= " " + time.text
+            print(str)
+        
+todaysMovies = displayFilms()
