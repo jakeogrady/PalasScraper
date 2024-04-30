bab33f6f2814e60f9e086b3518a7f65fdb9f2989
Jake <jakeogrady15@gmail.com>
2024-04-30 21:05:29 +0100

    Restarted Project

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from bs4 import BeautifulSoup
+from datetime import date
 import requests
+
+
 url = "https://www.palas.ie/"
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'lxml')
+today = date.today()
 
-def displayFilms():
-    list = soup.find_all("div", class_="film")
-    for film in list:
-        str = ""
-        title = film.find("h3").find("a")
-        
-        timeSlots = film.find("div",class_="times")
-        times = timeSlots.find_all("a")
-        if times:
-            str += title.text+":"
-            for time in times:
-                str+= " " + time.text
-            print(str)
-        
-todaysMovies = displayFilms()
+
+div = soup.find("div", class_="tab active loaded")
+films = div.find_all("div",class_="film")
+# this gets the list of films on today
+
+print(f'Movies for {today}:',end="")
+for film in films:
+    movieName = film.h3.a
+    movieTimes = film.find_all("div", class_="times")
+    
+    if not movieTimes[0].text:
+        continue
+    
+    print(f'\n{movieName.text}',end="")
+    for time in movieTimes:
+        print(time.text+" ")
